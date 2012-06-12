abc67bab49b174d94b6b0b8f9cabf84e349853a1
Caike Souza <carloshrsouza@gmail.com>
2012-06-12 17:16:48 -0400

    Initial Rails 3.2.5 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineCoffee::Application.config.secret_token = 'e4d3ef80712a40e0d99fe4a5f462afbf9e69cb8b554e2b3356e073606df5d5ac4db8aeff6befbbbf18a5dd3d10bafdf07b5ebffe50850b88af65ce4df0a2c3e7'
